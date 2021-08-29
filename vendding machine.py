da44418eddaf6019c3bb07b1ff0464ca971cc79e
froggal <77488170+froggal@users.noreply.github.com>
2021-08-29 19:31:59 +0900

    File Upload
    Vendding Machine File Upload

---
+++ vendding machine.py
@@ -0,0 +1,11 @@
+juice = input("어떤 음료수를 마시고 싶으신가요? 콜라, 사이다, 환타, 커피 중에서 선택해 주세요")
+if juice == "콜라" :
+    print("콜라가 나옵니다")
+elif juice == "사이다" :
+    print("사이다가 나옵니다")
+elif juice == "환타" :
+    print("환타가 나옵니다")
+elif juice == "커피" :
+    print("커피가 나옵니다")
+else :
+    print("그런 음료수는 없습니다.")
